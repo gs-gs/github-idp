887c040a34645332ec06f4d241d19c972553abfc
Kseniya Shychko <kseniya.shychko@gosource.com.au>
2024-12-09 23:19:05 +0100

    Chore update to node20 (#25)
    * chore: update to node20
    
    * fix: remove jasmin, user testTimeout option
    
    * fix: remove jasmin, user testTimeout option
    
    * fix: remove jasmin, user testTimeout option

--- src/github.pact.test.js
+++ src/github.pact.test.js
@@ -17,12 +17,10 @@ describe('With an increased jasmine timeout', () => {
     // We have to increase the jasmine timeout in order to run pact
     // on some machines / configurations, since the server sometimes
     // takes a little longer to start
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
   });
 
   afterAll(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   describe('GitHub Client Pact', () => {
@@ -60,12 +58,11 @@ describe('With an increased jasmine timeout', () => {
         });
 
         // add expectations
-        it('returns a sucessful body', done =>
+        it('returns a sucessful body', () =>
           github(PACT_BASE_URL)
             .getUserDetails('THIS_IS_MY_TOKEN')
             .then(response => {
               expect(response).toEqual(EXPECTED_BODY);
-              done();
             }));
       });
       describe('When the access token is bad', () => {
@@ -158,12 +155,11 @@ describe('With an increased jasmine timeout', () => {
         });
 
         // add expectations
-        it('returns a sucessful body', done =>
+        it('returns a sucessful body', () =>
           github(PACT_BASE_URL)
             .getUserEmails('THIS_IS_MY_TOKEN')
             .then(response => {
               expect(response).toEqual(EXPECTED_BODY);
-              done();
             }));
       });
       describe('When the access token is bad', () => {
@@ -288,12 +284,11 @@ describe('With an increased jasmine timeout', () => {
         });
 
         // add expectations
-        it('returns a sucessful body', done =>
+        it('returns a sucessful body', () =>
           github(PACT_BASE_URL)
             .getToken('SOME_CODE')
             .then(response => {
               expect(response).toEqual(EXPECTED_BODY);
-              done();
             }));
       });
       describe('When the code is bad', () => {
